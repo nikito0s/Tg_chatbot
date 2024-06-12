acb2e7575d66bf3f4a88f49bf9c6502ed09f3186
nikito0s <nikita.larionov.2004@outlook.com>
2024-06-12 17:14:24 +0300

    Delete bot-joke-master directory

--- bot-joke-master/src/main/java/ru/telgram/jokebot/repository/JokesRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.telgram.jokebot.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.telgram.jokebot.model.Joke;
-
-public interface JokesRepository extends JpaRepository<Joke, Long> {
-}
